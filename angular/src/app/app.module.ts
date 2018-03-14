8f9d3ca820189c633d86558dcd09b8699ce561e5
Elizabeth Martín Campos <eli@zabeth.es>
2018-03-14 20:31:30 +0100

    Started login form

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
+import { ConnectComponent } from './connect/connect.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/connect/connect.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-connect',
+  templateUrl: './connect.component.html',
+  styleUrls: ['./connect.component.css']
+})
+export class ConnectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
